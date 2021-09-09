8cbf5fd050ac9c482e0a05f4a6a142d43c82937a
Jhon Soplopuco <67161649+Jhon-20@users.noreply.github.com>
2021-09-09 15:59:55 -0500

    portafolio terminado

--- js/app.js
+++ js/app.js
@@ -7,16 +7,16 @@ function Scroll(x) {
     if (x.matches && window.scrollY == 0) {
       header.style.setProperty("background", "transparent"),
         header.style.setProperty("border-bottom", "none"),
-        header.style.setProperty("height", "4rem"),
-        header.style.setProperty("margin-top", "1em");
+        header.style.setProperty("height", "6rem");
     } else {
       header.style.setProperty("background", "#ffffff"),
         header.style.setProperty("border-bottom", "1px solid #EDEDED"),
         header.style.setProperty("margin-top", "0");
+      header.style.setProperty("height", "4rem");
     }
   });
 }
-var x = window.matchMedia("(min-width: 993px)");
+var x = window.matchMedia("(min-width: 992px)");
 Scroll(x);
 x.addListener(Scroll);
 let menu_bar = document.querySelector(".menu_bar");
